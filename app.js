2356db88483a7fd10d947f8e2672acf9ea911c55
Ayush Rameja <ayushrwork@gmail.com>
2021-04-23 19:19:43 +0530

    Improved animations and reduced file size

--- app.js
+++ app.js
@@ -1,59 +1,67 @@
-// selecting the elements
+// *selecting the elements
+const toggleSwitch = document.querySelector('#switch')
+const icon = document.querySelector('#status-icon')
+const title = document.querySelector('#title')
+const imgMain = document.querySelector('#img-main')
+const mainText = document.querySelector('#main-text')
+const imgStudy = document.querySelector('#img-study')
+const imgGame = document.querySelector('#img-game')
+const imgYoga = document.querySelector('#img-yoga')
+const nav = document.querySelector('#nav')
 
-const toggleSwitch = document.querySelector("#switch");
-const icon = document.querySelector("#status-icon");
-const title = document.querySelector("#title");
-const imgMain = document.querySelector("#img-main");
-const mainText = document.querySelector("#main-text");
-const imgStudy = document.querySelector("#img-study");
-const imgGame = document.querySelector("#img-game");
-const imgYoga = document.querySelector("#img-yoga");
-
-// functions
-
+// *functions
 function imageColor(color) {
-    imgMain.src = `./media/svg/theme-${color}.svg`;
-    imgStudy.src = `./media/svg/study-${color}.svg`;
-    imgGame.src = `./media/svg/gaming-${color}.svg`;
-    imgYoga.src = `./media/svg/yoga-${color}.svg`;
+  imgMain.src = `./media/svg/theme-${color}.svg`
+  imgStudy.src = `./media/svg/study-${color}.svg`
+  imgGame.src = `./media/svg/gaming-${color}.svg`
+  imgYoga.src = `./media/svg/yoga-${color}.svg`
 }
 
+// *dark mode
 function darkMode() {
-    icon.classList.replace("fa-sun", "fa-moon");
-    title.innerHTML = "Dark Mode";
-    mainText.innerHTML = "Dark Mode";
-    imageColor('dark');
+  icon.classList.replace('fa-sun', 'fa-moon')
+  title.innerHTML = 'Dark Mode'
+  mainText.innerHTML = 'Dark Mode'
+  imageColor('dark')
 }
-
+// *light mode
 function lightMode() {
-    icon.classList.replace("fa-moon", "fa-sun");
-    title.innerHTML = "Light Mode";
-    mainText.innerHTML = "Light Mode";
-    imageColor('light');
+  icon.classList.replace('fa-moon', 'fa-sun')
+  title.innerHTML = 'Light Mode'
+  mainText.innerHTML = 'Light Mode'
+  imageColor('light')
 }
-
+// *switching theme
 function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        darkMode();
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        lightMode();
-    }
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+    darkMode()
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+    lightMode()
+  }
 }
 
-// Event Listener
-toggleSwitch.addEventListener('change', switchTheme);
+// *EventListeners
+toggleSwitch.addEventListener('change', switchTheme)
 
-// Check Local Storage For Theme
-const currentTheme = localStorage.getItem('theme');
+// *nav animations
+window.onscroll = () => {
+  if (window.pageYOffset > 0) {
+    nav.classList.add('scrolled')
+  } else {
+    nav.classList.remove('scrolled')
+  }
+}
+// *Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme')
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute('data-theme', currentTheme)
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        darkMode();
-    }
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true
+    darkMode()
+  }
 }
